e955ef42d61e823e662ea8fd1a44e99100645824
arnaldoajf <arnald.felipe@gmail.com>
2025-12-19 16:48:34 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let heroi = "Dio.me"
+let nivelHeroiXp = 2002
+
+if (nivelHeroiXp <= 1000) {
+    console.log("O Herói de nome " + heroi + " está no nível de Ferro")
+}
+else if (nivelHeroiXp >= 1001 && nivelHeroiXp <= 2000) {
+    console.log("O Herói de nome " + heroi + " está no nível de Bronze")
+}
+else if (nivelHeroiXp >= 2001 && nivelHeroiXp <= 5000) {
+    console.log("O Herói de nome " + heroi + " está no nível de Prata")
+}
+else if (nivelHeroiXp >= 5001 && nivelHeroiXp <= 7000) {
+    console.log("O Herói de nome " + heroi + " está no nível de Ouro")
+}
+else if (nivelHeroiXp >= 7001 && nivelHeroiXp <= 8000) {
+    console.log("O Herói de nome " + heroi + " está no nível de Platina")
+}
+else if (nivelHeroiXp >= 8001 && nivelHeroiXp <= 9000) {
+    console.log("O Herói de nome " + heroi + " está no nível de Ascendente")
+}
+else if (nivelHeroiXp >= 9001 && nivelHeroiXp <= 10000) {
+    console.log("O Herói de nome " + heroi + " está no nível de Imortal")
+}
+else {
+    console.log("O Herói de nome " + heroi + " está no nível de Radiante")
+}
